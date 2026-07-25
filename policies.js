4817e711ad0072d4f0afcc3d685ccd59b1ddd767
akilluminati47 <acaradimos@gmail.com>
2026-07-25 15:56:04 -0400

    Get download buttons by explicit IDs instead of querySelector

--- policies.js
+++ policies.js
@@ -335,7 +335,7 @@ addEventListener('hashchange', ()=>{ if(location.hash==='#policies' && !isOpen()
 function navGrid(){
   const ptiles = [...document.querySelectorAll('.ptile')];
   const ppips = [...document.querySelectorAll('.ppip')];
-  const pdownBtns = [...document.querySelectorAll('.pdown-btn')];
+  const pdownBtns = ['pdown-exe','pdown-apk','pdown-ipa'].map(id => document.getElementById(id)).filter(Boolean);
   const ghbadge = document.querySelector('.pdown-ghbadge');
   const ptoblock = document.getElementById('ptoblock');
   const pcredit = document.getElementById('pcreditlink');
